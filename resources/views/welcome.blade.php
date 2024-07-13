a60ed60e5d0fc932af4b519308d2c93959901956
rudyhuacasi <160580742+rudyhuacasi@users.noreply.github.com>
2024-07-13 21:23:59 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel template</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <h1>laravel template</h1>
+
+    <img src="{{ Vite::asset('resources/img/kaneki.jpg') }}" alt="paperella lavarel template">
+</body>
+
+</html>
